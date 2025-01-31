3b8126532d0176482233f9f291abbd6c837c85ac
Carlos <cjworkk42@gmail.com>
2025-01-31 08:43:59 -0300

    Update and rename README.md to Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,28 @@
+# Sequencia-de-Fibonacci
+Apenas um exercício de Lógica em Python que apresenta os 10 primeiros termos da sequencia de Fibonacci e posteriormente pergunta ao usuário quantos a mais ele quer ver. 
+
+
+c = 1
+valor = 1
+antecessor = 0
+mais = 10
+total = 0
+
+
+while mais != 0 :
+    total += mais 
+
+    while c <= total :
+        print (f'O {c} e {c + 1} termos da sequencia são: {antecessor} e {valor}.')
+
+
+
+        antecessor = antecessor + valor 
+        valor = valor + antecessor 
+        c += 1
+
+    print ('PAUSA')
+    mais = int(input('Quantos termos a mais você quer ver?'))
+
+
+print ('O programa acabou. OBRIGADO!')
--- README.md
+++
@@ -1,2 +0,0 @@
-# Sequencia-de-Fibonacci
-Apenas um exercício de Lógica em Python que apresenta os 10 primeiros termos da sequencia de Fibonacci e posteriormente pergunta ao usuário quantos a mais ele quer ver. 
