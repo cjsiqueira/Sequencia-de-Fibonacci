ec9d62b1d173f2cb2855834e8c997ccc620b1192
Carlos <cjworkk42@gmail.com>
2025-01-31 08:44:32 -0300

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,5 @@
 # Sequencia-de-Fibonacci
-Apenas um exercício de Lógica em Python que apresenta os 10 primeiros termos da sequencia de Fibonacci e posteriormente pergunta ao usuário quantos a mais ele quer ver. 
+#Apenas um exercício de Lógica em Python que apresenta os 10 primeiros termos da sequencia de Fibonacci e posteriormente pergunta ao usuário quantos a mais ele quer ver. 
 
 
 c = 1
